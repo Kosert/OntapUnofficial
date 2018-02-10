3be7d1e31cfada4aac3716b43c59f63be8191614
kosert <robert.kosakowski@student.put.poznan.pl>
2018-02-10 12:33:17 +0100

    initial commit

---
+++ app/src/main/java/me/kosert/ontap/ui/activities/main/MainActivity.kt
@@ -0,0 +1,23 @@
+package me.kosert.ontap.ui.activities.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.main_activity.*
+import me.kosert.ontap.R
+
+/**
+ * Created by Kosert on 2018-02-10.
+ */
+class MainActivity : AppCompatActivity()
+{
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		setTheme(R.style.AppTheme_NoActionBar)
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.main_activity)
+
+		setSupportActionBar(main_toolbar)
+
+
+	}
+}
---
+++ app/src/main/java/me/kosert/ontap/ui/activities/main/MainPagerAdapter.kt
@@ -0,0 +1,9 @@
+package me.kosert.ontap.ui.activities.main
+
+/**
+ * Created by Kosert on 2018-02-10.
+ */
+class MainPagerAdapter : 
+{
+
+}
